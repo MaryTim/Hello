d3b60a757feeba6effae1457627dc93d5d9d1f1b
MaryTim <61231496+MaryTim@users.noreply.github.com>
2020-02-26 21:08:05 +0300

    changes in February

---
+++ src/l03/PrimeNumbers.java
@@ -0,0 +1,18 @@
+package l03;
+
+import java.util.Random;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        Random random = new Random ( );
+        int n = random.nextInt ( 1000 );
+        for (int i = 2; i < n; i++) {
+            if ( n % i == 0 ) {
+                System.out.println ( n + " не простое" );
+                return;
+            }
+        }
+        System.out.println ( n + " простое" );
+    }
+}
+
