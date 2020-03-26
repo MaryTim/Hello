6339acbfc9df9e579f79d5b87bd8a930377b6258
MaryTim <61231496+MaryTim@users.noreply.github.com>
2020-03-26 12:33:39 +0300

    initial commit

--- src/l13/Addition.java
+++ src/l13/Addition.java
@@ -3,20 +3,33 @@
 
 package l13;
 
+import java.time.Duration;
+import java.time.LocalTime;
+
 public class Addition {
 
     public static void demonstrateSimpleAddition () {
 
         String str1 = "Hello!";
+        LocalTime t1 = LocalTime.now();
         str1 += " How are you?";
         str1 += " How's the weather?";
         System.out.println(str1);
+        LocalTime t2 = LocalTime.now();
+        Duration between = Duration.between(t2, t1);
+        long duration = between.getNano();
+        System.out.println(duration);
     }
 
     public static void demonstrateStringBuilder () {
 
         StringBuilder sb = new StringBuilder("Hello!");
+        LocalTime t1 = LocalTime.now();
         sb.append(" How are you? How's the weather?");
         System.out.println(sb);
+        LocalTime t2 = LocalTime.now();
+        Duration between = Duration.between(t2, t1);
+        long duration = between.getNano();
+        System.out.println(duration);
     }
 }
